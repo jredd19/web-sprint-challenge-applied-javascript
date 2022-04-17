f1743c1bb316a4ab1450112e026fe22b27b95b19
jamel <jay.redd19@gmail.com>
2022-04-17 17:41:21 -0400

    task 1 complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,30 @@
 const Header = (title, date, temp) => {
+
+  // creating elements
+  const header = document.createElement('div');
+  const headerTitle = document.createElement('h1');
+  const headerDate = document.createElement('span');
+  const headerTemp = document.createElement('span');
+
+  // creating hierarchy
+  header.appendChild(headerTitle);
+  header.appendChild(headerDate);
+  header.appendChild(headerTemp);
+
+  // setting element classes
+  header.classList.add('header');
+  headerTitle.classList.add('header-title');
+  headerDate.classList.add('header-date');
+  headerTemp.classList.add('header-temp');
+
+// adding text 
+  headerTitle.textContent = title;
+  headerDate.textContent = date;
+  headerTemp.textContent = temp;
+
+  return header;
+
+
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -13,7 +39,14 @@ const Header = (title, date, temp) => {
   //
 }
 
+Header();
+
 const headerAppender = (selector) => {
+
+  // setting entry point
+const entryPoint = document.querySelector(selector);
+axios.get(Header);
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
@@ -21,5 +54,7 @@ const headerAppender = (selector) => {
   // It should append the header to the element in the DOM that matches the given selector.
   //
 }
+
+console.log(headerAppender('.header-container'))
 
 export { Header, headerAppender }
